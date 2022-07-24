a1d87c0a23eeffe16bad2aa53815045fda6cd5ab
Setaganga <officialfossilmilk@gmail.com>
2022-07-24 16:58:54 -0600

    7/24 Git Setup

---
+++ HomePage/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    htmlpage = loader.get_template('Home.html')
+    return HttpResponse(htmlpage.render())
+
+def login(request):
+    htmlpage = loader.get_template('login.html')
+    return HttpResponse(htmlpage.render())
